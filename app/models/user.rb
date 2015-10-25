9c182fbcdd1b148f57be9990cb3f6d7d94e6d11e
ariekdev <ariekdev@gmail.com>
2015-10-25 21:30:22 +0300

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     puts "from_omniauth"
-    p auth:
+    p auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
