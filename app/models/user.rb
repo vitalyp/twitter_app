d3555180d6d65f8b59e77a4f2b468e543fca8e51
ariekdev <ariekdev@gmail.com>
2015-10-25 21:25:46 +0300

    add omniauth logging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
+    puts "from_omniauth"
+    p auth:
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      p "well done!"
       user.save
     end
   end
