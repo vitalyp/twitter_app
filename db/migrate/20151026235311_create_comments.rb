dbdab3b1ae672dbd3d7e73f87de1b30ee351af2a
ariekdev <ariekdev@gmail.com>
2015-10-27 06:33:41 +0300

    add comments

---
+++ db/migrate/20151026235311_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.string :status
+      t.references :user, index: true, foreign_key: true
+      t.references :tweet, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
