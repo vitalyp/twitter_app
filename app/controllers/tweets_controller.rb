772a8f17f39cff941af24ee50750f0baf28d7f78
ariekdev <ariekdev@gmail.com>
2015-10-27 06:58:37 +0300

    rewrite logic to figureout heroku error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,14 +10,15 @@ class TweetsController < ApplicationController
     @tweet.user = current_user
     if @tweet.save
       begin
-        Pusher.trigger('tweet_channel', 'new_tweet', {
-          tweet_id: @tweet.id,
-          tweet_name: @tweet.name,
-          user_name: @tweet.user.short_name,
-          name: @tweet.name,
-          user_picture_url: @tweet.user.picture_url
-        })
+        tweet_id = @tweet.id
+        tweet_name = @tweet.name
+        user_name = @tweet.user.short_name
+        name = @tweet.name
+        user_picture_url = @tweet.user.picture_url
+        Pusher.trigger('tweet_channel', 'new_tweet', {tweet_id: tweet_id,tweet_name: tweet_name,user_name: user_name,name: name,user_picture_url: user_picture_url})
       rescue Pusher::Error => e
+        p "___PUSHER ERROR___:"
+        p e
         status = e
       end
       render :json => { status: e }
