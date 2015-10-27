879d08b86143988eeb7dc50cf067da57f2fcc77d
ariekdev <ariekdev@gmail.com>
2015-10-27 11:19:19 +0300

    implement destroy tweets

--- app/assets/javascripts/tweets.coffee
+++ app/assets/javascripts/tweets.coffee
@@ -5,6 +5,7 @@
 $ ->
   $("#post_btn").click ->
     post_name = $("#post_name").val()
+    $('#post_name').val("")
     if post_name.length > 100
       alert "post is too long, make it shorter!"
     else
@@ -16,3 +17,18 @@ $ ->
           name: post_name
         success: (resp) ->
           console.log resp
+
+  $('#post_name').keyup (event) ->
+    if event.keyCode == 13
+      $('#post_btn').click()
+    return
+
+  $('.trash').click (e) ->
+    if confirm('Are you sure you want to delete this Tweet?')
+      tweet_id = $(this).data('tweet_id')
+      $.ajax
+        url: '/tweets/'+tweet_id
+        type: 'DELETE'
+        success: (resp) ->
+          console.log resp
+    return
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,32 +1,45 @@
 class TweetsController < ApplicationController
   before_action :require_login
 
+  include ActionView::Helpers::DateHelper
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.limit(10).order("created_at desc")
   end
 
   def create
     @tweet = Tweet.new tweet_params
     @tweet.user = current_user
     if @tweet.save
+      status = 'true'
       begin
-        tweet_id = @tweet.id
-        tweet_name = @tweet.name
-        user_name = @tweet.user.short_name
-        user_picture_url = @tweet.user.picture_url
-
-        p "tweet_id: #{tweet_id}"
-        p "tweet_name: #{tweet_name}"
-        p "user_name: #{user_name}"
-        p "user_picture_url: #{user_picture_url}"
-
-        Pusher.trigger('tweet_channel', 'new_tweet', {tweet_id: tweet_id||"",tweet_name: tweet_name||"",user_name: user_name||"",user_picture_url: user_picture_url||""})
+        Pusher.trigger('tweet_channel', 'new_tweet', {
+          tweet_id: @tweet.id,
+          tweet_name: @tweet.name,
+          user_name: @tweet.user.short_name,
+          user_picture_url: @tweet.user.picture_url
+        })
       rescue Pusher::Error => e
-        p "___PUSHER ERROR___:"
         p e
         status = e
       end
-      render :json => { status: e }
+      render :json => { status: status }
+    else
+      render :json => { status: 'false' }, :status => 500
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet && @tweet.user == current_user
+      @tweet.destroy
+      status = 'true'
+      begin
+        Pusher.trigger('tweet_channel', 'destroy_tweet', { tweet_id: params[:id]})
+      rescue Pusher::Error => e
+        p e
+        status = e
+      end
+      render :json => { status: status }
     else
       render :json => { status: 'false' }, :status => 500
     end
