0777a798c881056e05b0f3d11efaf1d902321d51
ariekdev <ariekdev@gmail.com>
2015-10-27 06:36:00 +0300

    implement create new tweet

--- app/assets/javascripts/tweets.coffee
+++ app/assets/javascripts/tweets.coffee
@@ -1,3 +1,18 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$ ->
+  $("#post_btn").click ->
+    post_name = $("#post_name").val()
+    if post_name.length > 100
+      alert "post is too long, make it shorter!"
+    else
+      console.log post_name
+      $.ajax
+        url: '/tweets'
+        type: 'POST'
+        data: tweet:
+          name: post_name
+        success: (resp) ->
+          console.log resp
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,34 @@ class TweetsController < ApplicationController
   before_action :require_login
 
   def index
+    @tweets = Tweet.all
+  end
 
+  def create
+    @tweet = Tweet.new tweet_params
+    @tweet.user = current_user
+    if @tweet.save
+      begin
+        Pusher.trigger('tweet_channel', 'new_tweet', {
+          tweet_id: @tweet.id,
+          tweet_name: @tweet.name,
+          user_name: @tweet.user.short_name,
+          name: @tweet.name,
+          user_picture_url: @tweet.user.picture_url
+        })
+      rescue Pusher::Error => e
+        status = e
+      end
+      render :json => { status: e }
+    else
+      render :json => { status: 'false' }, :status => 500
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:name)
   end
 
 end
