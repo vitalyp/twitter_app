15378ba428f330a295be8804a3068ca11e0baee2
Vitaly Pestov <ariekdev@gmail.com>
2015-11-26 00:25:41 +0000

    create GOOGLE_REDIRECT_URL env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ G_APP_ID = ENV['GOOGLE_APP_ID']
 G_APP_SECRET = ENV['GOOGLE_APP_SECRET']
 
 if Rails.env.production?
-  GOOGLE_REDIRECT_URL = 'https://vitalyp-twitter-app.herokuapp.com/auth/google/callback'
+  GOOGLE_REDIRECT_URL = ENV['GOOGLE_REDIRECT_URL'] || 'https://vitalyp-twitter-app.herokuapp.com/auth/google/callback'
 else
   GOOGLE_REDIRECT_URL = 'http://local.twitter.com:3000/auth/google/callback'
   OmniAuth.config.full_host = 'http://local.twitter.com:3000'
