aa7483bcec84a0b5a90857e0c017da96e75a7943
vpiest889 <Vitalii_Piestov@comcast.com>
2021-06-30 05:59:25 +0300

    fix constroller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login
-  before_filter :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
     current_user.build_profile if current_user.profile.nil?
