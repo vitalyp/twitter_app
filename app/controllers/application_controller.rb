fbfec1ee5982dcb4114212474c9ac02e185184dd
vpiest889 <Vitalii_Piestov@comcast.com>
2021-06-30 05:12:04 +0300

    upd2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    User.first
-    #@current_user ||= User.find(session[:user_id]) if session[:user_id]
+    #User.first
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
   helper_method :current_user
